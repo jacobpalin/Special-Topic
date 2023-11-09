51328799543d64d55551930f65b4c819246baf68
Jacob Palin <jacobcpalin@gmail.com>
2023-11-09 12:41:13 -0700

    comments added to code

--- Special Topic/Assets/UMACharacterEditor.cs
+++ Special Topic/Assets/UMACharacterEditor.cs
@@ -10,11 +10,12 @@ public class UMACharacterEditor : MonoBehaviour
 {
     public DynamicCharacterAvatar avatar;
     private Dictionary<string, DnaSetter> dna;
-    public Transform sliders;
+    public Transform sliders; //Empty game object that has all sliders as children
 
     void OnEnable()
     {
         avatar.CharacterUpdated.AddListener(Updated);
+        //add listener for every child in the sliders object
         foreach (Transform child in sliders.transform)
         {
             child.gameObject.GetComponent<Slider>().onValueChanged.AddListener(OnSliderChanged);
@@ -25,7 +26,7 @@ public class UMACharacterEditor : MonoBehaviour
     {
         avatar.CharacterUpdated.RemoveListener(Updated);
     }
-
+    //initially updates the UI sliders to match what value they should be at when starting
     void Updated(UMAData data)
     {
         dna = avatar.GetDNA();
@@ -34,7 +35,7 @@ public class UMACharacterEditor : MonoBehaviour
             child.gameObject.GetComponent<Slider>().value = dna[child.gameObject.name].Get();
         }
     }
-
+    //uses value from sliders and the name of the current slider selected to manipulate that part of the model
     public void OnSliderChanged(float val)
     {
         var go = EventSystem.current.currentSelectedGameObject;
